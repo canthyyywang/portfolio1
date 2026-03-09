21501fdc44a03ec858105bb9e00d4194b630e332
王启萌 <canthyyy@wangqimengdeMacBook-Pro.local>
2026-03-09 14:09:01 +0800

    启用静态导出模式

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export", // 启用静态导出
+  images: {
+    unoptimized: true, // 如果你用了 next/image，需要加上这个
+  },
+};
 
 export default nextConfig;
